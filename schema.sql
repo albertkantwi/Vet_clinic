aaa56a74182d06130828cc8a0ce969834961835d
unknown <albertkantwi@gmail.com>
2023-09-30 15:34:44 +0100

    Add new tables to vet_clinic

--- schema.sql
+++ schema.sql
@@ -33,3 +33,24 @@ ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) R
 
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(100) NOT NULL,
+age INT NOT NULL,
+date_of_graduation DATE);
+
+CREATE TABLE specializations(
+vets_id INT NOT NULL,
+species_id INT NOT NULL,
+PRIMARY KEY(vets_id, species_id),
+CONSTRAINT fk_specializatino_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+CONSTRAINT fk_specialization_species FOREIGN KEY(species_id) REFERENCES species(id));
+
+CREATE TABLE visits (
+animal_id INT NOT NULL,
+vet_id INT NOT NULL,
+date_of_visitation DATE,
+PRIMARY KEY (animal_id, vet_id, date_of_visitation),
+CONSTRAINT fk_visit_animal FOREIGN KEY(animal_id) REFERENCES animals(id),
+CONSTRAINT fk_visit_vet FOREIGN KEY(vet_id) REFERENCES vets(id));
