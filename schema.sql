4acfaecad5f93563b897adbcd3f4bf52438fca28
unknown <albertkantwi@gmail.com>
2023-09-30 09:53:09 +0100

    Update tables

--- schema.sql
+++ schema.sql
@@ -20,10 +20,24 @@ id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 full_name VARCHAR(100) NOT NULL,
 age INT NOT NULL);
 
-ALTER TABLE animals DROP COLUMN species;
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+);
 
-ALTER TABLE animals ADD COLUMN species_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals
+ADD PRIMARY KEY (id),
+DROP COLUMN species;
 
-ALTER TABLE animals ADD COLUMN owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_animals_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT species_fkey
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT owners_fkey
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
